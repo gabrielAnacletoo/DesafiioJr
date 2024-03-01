ab16c3575ac95d1d8cfa2b74031f5f7b57caddaf
Gabriel Anacleto <gabrielanacleto159@live.com>
2024-03-01 09:43:09 -0300

    revisando

--- src/steps/findAllItems/filter/filterProducts.ts
+++ src/steps/findAllItems/filter/filterProducts.ts
@@ -1,4 +1,4 @@
-import { PuppeteerService } from 'src/steps/accessWebsite/puppeteer.config';
+import { PuppeteerService } from '../../accessWebsite/puppeteer.config';
 
 export class ProductFilterFacade {
   constructor(private readonly puppeteerService: PuppeteerService) {}
@@ -11,44 +11,48 @@ export class ProductFilterFacade {
       this.puppeteerService.setupConsoleListener((msg) => {
         console.log(msg.text());
       });
-
+    
       console.log('Filtrando produtos por nutrição e pontuação Nova...');
-      const products = await this.puppeteerService.evaluate(() => {
+      const filteredProducts = await this.puppeteerService.evaluate((nutrition, nova) => {
         const productList = Array.from(document.querySelectorAll('#products_match_all li'));
         console.log('Extraindo informações dos produtos...');
+    
+        console.log('Formatando a pontuação')
+        const extractScore = (iconTitle) => {
+          const [_, score] = iconTitle.split(' ');
+          return score.charAt(0);
+        };
 
-        return productList.map((item) => {
+        const filtered = [];
+        productList.forEach((item) => {
           const productNutriScoreIcon = item.querySelector('.list_product_icons[title^="Nutri-Score"]');
           const productNutriScoreTitle = productNutriScoreIcon ? productNutriScoreIcon.getAttribute('title') : '';
-          const productNutriScore = productNutriScoreTitle.split(' ')[1].charAt(0)
-
+          const productNutriScore = extractScore(productNutriScoreTitle);
+    
           const productNovaIcon = item.querySelector('.list_product_icons[title^="NOVA"]');
           const productNovaTitle = productNovaIcon ? productNovaIcon.getAttribute('title') : '';
-          const productNovaScore = productNovaTitle.split(' ')[1].charAt(0);
-
+          const productNovaScore = extractScore(productNovaTitle);
+    
           const productIdElement = item.querySelector('a.list_product_a');
           const productId = productIdElement ? productIdElement.getAttribute('href').split('/').slice(-2, -1)[0] : '';
-
-          return {
-            productId,
-            productName: item.querySelector('.list_product_name').textContent,
-            productNutriScoreTitle,
-            productNutriScore,
-            productNovaTitle,
-            productNovaScore,
-          };
+    
+          if (productNutriScore === nutrition && productNovaScore === nova) {
+            filtered.push({
+              productId,
+              productName: item.querySelector('.list_product_name').textContent,
+              productNutriScoreTitle,
+              productNutriScore,
+              productNovaTitle,
+              productNovaScore,
+            });
+          }
         });
-      });
-
-      if (products) {
-        console.log('Produtos encontrados e informações extraídas com sucesso.');
-      }
-
-      return products.filter(
-        (product) =>
-          product.productNutriScore === nutrition &&
-          product.productNovaScore === nova,
-      );
+    
+        return filtered;
+      }, nutrition, nova);
+    
+      console.log('Produtos encontrados e informações extraídas com sucesso.');
+      return filteredProducts;    
     } finally {
       await this.puppeteerService.close();
     }
